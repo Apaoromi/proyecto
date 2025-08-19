96c6a68af96012336195c1918dff713f13d2c23f
apaoromi <oromiapa@gmail.com>
2025-08-19 03:23:40 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -9,7 +9,7 @@ fetch(apiURL)
       html += `
         <div class="product-card">
           <img src="${product.image}" alt="${product.name}">
-          <a href="product-info.html?id=${product.id}">${product.name}</a>
+          <a id="nomm" href="product-info.html?id=${product.id}">${product.name}</a>
           <p>${product.description}</p>
           <div class="price">${product.currency} ${product.cost}</div>
           <div class="sold">Vendidos: ${product.soldCount}</div>
