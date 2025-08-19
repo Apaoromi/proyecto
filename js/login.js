e0820bb4aaf1f6f330301a840890151b6ee9b44d
apaoromi <oromiapa@gmail.com>
2025-08-19 01:26:09 -0300

    prodructs.js

--- js/login.js
+++ js/login.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
 document.getElementById("formRegistro").addEventListener("submit", function(event) {
   event.preventDefault(); 
 
