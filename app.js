f1f83b48727a436f487ca9810a71401059f54c33
apaoromi <oromiapa@gmail.com>
2025-11-24 02:01:49 -0300

    a

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Servir archivos estáticos desde la carpeta actual
+app.use(express.static(__dirname));
+
+// Ruta principal
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
+});
